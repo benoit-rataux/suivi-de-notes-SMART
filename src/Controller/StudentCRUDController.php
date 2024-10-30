9d0cc7065f9d2c40a1099fc59e2b466aa80f6cd3
Benoît Rataux <benoit.rataux@gmail.com>
2024-10-30 07:47:28 -0400

    #FIX - path "app_student_crud_"->"app_student_"

--- src/Controller/StudentCRUDController.php
+++ src/Controller/StudentCRUDController.php
@@ -11,22 +11,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/student', name:'app_student_')]
-final class StudentCRUDController extends AbstractController
-{
+#[Route('/student', name: 'app_student_')]
+final class StudentCRUDController extends AbstractController {
     #[Route(name: 'index', methods: ['GET'])]
-    public function index(StudentRepository $studentRepository): Response
-    {
-        return $this->render('student_crud/index.html.twig', [
+    public function index(StudentRepository $studentRepository): Response {
+        return $this->render('student/index.html.twig', [
             'students' => $studentRepository->findAll(),
         ]);
     }
 
     #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
+    public function new(Request $request, EntityManagerInterface $entityManager): Response {
         $student = new Student();
-        $form = $this->createForm(StudentType::class, $student);
+        $form    = $this->createForm(StudentType::class, $student);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -36,23 +33,21 @@ final class StudentCRUDController extends AbstractController
             return $this->redirectToRoute('app_student_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('student_crud/new.html.twig', [
+        return $this->render('student/new.html.twig', [
             'student' => $student,
-            'form' => $form,
+            'form'    => $form,
         ]);
     }
 
     #[Route('/{id}', name: 'show', methods: ['GET'])]
-    public function show(Student $student): Response
-    {
-        return $this->render('student_crud/show.html.twig', [
+    public function show(Student $student): Response {
+        return $this->render('student/show.html.twig', [
             'student' => $student,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Student $student, EntityManagerInterface $entityManager): Response
-    {
+    public function edit(Request $request, Student $student, EntityManagerInterface $entityManager): Response {
         $form = $this->createForm(StudentType::class, $student);
         $form->handleRequest($request);
 
@@ -62,16 +57,15 @@ final class StudentCRUDController extends AbstractController
             return $this->redirectToRoute('app_student_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('student_crud/edit.html.twig', [
+        return $this->render('student/edit.html.twig', [
             'student' => $student,
-            'form' => $form,
+            'form'    => $form,
         ]);
     }
 
     #[Route('/{id}', name: 'delete', methods: ['POST'])]
-    public function delete(Request $request, Student $student, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$student->getId(), $request->getPayload()->getString('_token'))) {
+    public function delete(Request $request, Student $student, EntityManagerInterface $entityManager): Response {
+        if ($this->isCsrfTokenValid('delete' . $student->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($student);
             $entityManager->flush();
         }
