408b26af84846ee4f6df0d19cba64a7bbd8b8b28
Benoît Rataux <benoit.rataux@gmail.com>
2024-11-06 08:18:20 -0500

    #REFAC - moved HomeController to Controller/html/
    Signed-off-by: Benoît Rataux <benoit.rataux@gmail.com>

--- src/Controller/html/HomeController.php
+++ src/Controller/html/HomeController.php
@@ -1,17 +1,15 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\html;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/', name: 'app_')]
-class HomeController extends AbstractController
-{
+class HomeController extends AbstractController {
     #[Route(name: 'home')]
-    public function index(): Response
-    {
+    public function index(): Response {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
