b2a257d42ef2808b66aaacb20fd3ebe9f2bb9bc8
Benoît Rataux <benoit.rataux@gmail.com>
2024-11-20 15:10:59 -0500

    FEAT - ajout entity SchoolClass
    Signed-off-by: Benoît Rataux <benoit.rataux@gmail.com>

--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -8,86 +8,89 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: StudentRepository::class)]
-class Student
-{
+class Student {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
+    
     #[ORM\Column(length: 50)]
     private ?string $firstname = null;
-
+    
     #[ORM\Column(length: 50)]
     private ?string $surname = null;
-
+    
     /**
      * @var Collection<int, Evaluation>
      */
     #[ORM\OneToMany(targetEntity: Evaluation::class, mappedBy: 'student')]
     private Collection $evaluations;
-
-    public function __construct()
-    {
+    
+    #[ORM\ManyToOne(inversedBy: 'students')]
+    private ?SchoolClass $schoolClass = null;
+    
+    public function __construct() {
         $this->evaluations = new ArrayCollection();
     }
-
-    public function getId(): ?int
-    {
+    
+    public function getId(): ?int {
         return $this->id;
     }
-
-    public function getFirstname(): ?string
-    {
+    
+    public function getFirstname(): ?string {
         return $this->firstname;
     }
-
-    public function setFirstname(string $firstname): static
-    {
+    
+    public function setFirstname(string $firstname): static {
         $this->firstname = $firstname;
-
+        
         return $this;
     }
-
-    public function getSurname(): ?string
-    {
+    
+    public function getSurname(): ?string {
         return $this->surname;
     }
-
-    public function setSurname(string $surname): static
-    {
+    
+    public function setSurname(string $surname): static {
         $this->surname = $surname;
-
+        
         return $this;
     }
-
+    
     /**
      * @return Collection<int, Evaluation>
      */
-    public function getEvaluations(): Collection
-    {
+    public function getEvaluations(): Collection {
         return $this->evaluations;
     }
-
-    public function addEvaluation(Evaluation $evaluation): static
-    {
-        if (!$this->evaluations->contains($evaluation)) {
+    
+    public function addEvaluation(Evaluation $evaluation): static {
+        if(!$this->evaluations->contains($evaluation)) {
             $this->evaluations->add($evaluation);
             $evaluation->setStudent($this);
         }
-
+        
         return $this;
     }
-
-    public function removeEvaluation(Evaluation $evaluation): static
-    {
-        if ($this->evaluations->removeElement($evaluation)) {
+    
+    public function removeEvaluation(Evaluation $evaluation): static {
+        if($this->evaluations->removeElement($evaluation)) {
             // set the owning side to null (unless already changed)
-            if ($evaluation->getStudent() === $this) {
+            if($evaluation->getStudent() === $this) {
                 $evaluation->setStudent(null);
             }
         }
-
+        
+        return $this;
+    }
+    
+    public function getSchoolClass(): ?SchoolClass {
+        return $this->schoolClass;
+    }
+    
+    public function setSchoolClass(?SchoolClass $schoolClass): static {
+        $this->schoolClass = $schoolClass;
+        
         return $this;
     }
 }
