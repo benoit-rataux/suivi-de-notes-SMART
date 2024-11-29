e06cd5d76e203335eb95a14d2e165db0448b4453
Benoît Rataux <benoit.rataux@gmail.com>
2024-11-29 13:33:52 -0500

    #Annulation
    Signed-off-by: Benoît Rataux <benoit.rataux@gmail.com>

--- src/Controller/Template/HomeController.php
+++ src/Controller/Template/HomeController.php
@@ -8,10 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/', name: 'app_')]
 class HomeController extends AbstractController {
+
     #[Route(name: 'home')]
     public function index(): Response {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
+
 }
